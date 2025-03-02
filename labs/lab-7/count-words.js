df26df2302f439c44fbdc72af9d8097927083c3e
Hieu <jikarintu@gmail.com>
2025-03-02 16:43:33 +0700

    Add lab 7

---
+++ labs/lab-7/count-words.js
@@ -0,0 +1,19 @@
+// Function to count the number of words in a string
+
+const readlineSync = require('readline-sync');
+const userStr = readlineSync.question("Input your string: ");
+// const str = "Hello ban, tui ten Teo, ban cung cung ten Teo luon ah";
+countWords();
+
+function countWords() {
+    const words = userStr.toLowerCase().replace(/[,.!?]/g, "").split(" ");
+
+    // Sử dụng một object để đếm số lần xuất hiện của từng từ
+    const wordCount = {};
+
+    words.forEach(word => {
+        wordCount[word] = (wordCount[word] || 0) + 1;
+    });
+
+    console.log(wordCount);
+}
